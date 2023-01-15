f09308d40404d24832677f8a0e2b6e644828cb54
OrkOrk435 <orkork435@users.noreply.github.com>
2023-01-15 23:17:34 +0100

    Basic update
    Settings window shows and hides now

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,12 +1,19 @@
 let settingsMenuShown = true;
-function showSetings()
-{
-    if(settingsMenuShown)
-    document.getElementById("settings-window").style.display="none";
-    else
-    document.getElementById("settings-window").style.display="block";
-}
 
 document.onkeydown = function (e) {
-    if(e.key = "x") showSettings();
+    if(e.key = "x") showSettings()
   };
+function showSettings()
+{
+    if(settingsMenuShown)
+    {
+        document.getElementById("settings-window").style.display="none";
+        settingsMenuShown = false;
+    }
+
+    else
+    {
+        document.getElementById("settings-window").style.display="block";
+        settingsMenuShown = true;
+    }
+}
