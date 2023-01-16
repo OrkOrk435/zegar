3e0e3959fa848b84ebf77f959232b2a827c88ec0
OrkOrk435 <orkork435@users.noreply.github.com>
2023-01-16 18:33:16 +0100

    Basic update
    Added image upload (position does not work yet)

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -6,7 +6,8 @@ function onStart()
 }
 
 document.onkeydown = function (e) {
-    if(e.key = "x") showSettings()
+    console.log(e.code);
+    if(e.key == "x") showSettings();
   };
 function showSettings()
 {
@@ -33,3 +34,23 @@ function modifySettings()
 
     setTimeout(modifySettings, 1);
 }
+
+
+$(document).ready(function(){
+
+    $('input.img-file').on('change', addFile);
+   
+});
+
+    function addFile (event) {
+
+       var jqDisplay = $('div.clock-logo');
+       var reader = new FileReader();
+       var selectedFile = event.target.files[0];
+
+       reader.onload = function (event) {
+           var imageSrc = event.target.result;
+           jqDisplay.html('<img src="' + imageSrc + '" alt="uploaded Image" width="'+document.getElementById("img-width").value+'" height="'+document.getElementById("img-height").value+'" style = "position: absolute;">');
+       };
+      reader.readAsDataURL(selectedFile); 
+   }
